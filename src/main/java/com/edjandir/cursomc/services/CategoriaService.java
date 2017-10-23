3c7256fa14768a24f802d8d74713a2cfe69529b6
Edjandir Corrêa Costa <edjandir@gmail.com>
2017-10-23 10:58:04 -0200

    criado repository e service para categoria

---
+++ src/main/java/com/edjandir/cursomc/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.edjandir.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.edjandir.cursomc.domain.Categoria;
+import com.edjandir.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Categoria obj = repo.findOne(id);
+		return obj;
+	}
+	
+}
