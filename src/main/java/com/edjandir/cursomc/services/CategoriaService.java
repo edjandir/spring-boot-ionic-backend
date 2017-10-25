0240d55fbb201e3743967d97e33fe5a92ae41aeb
Edjandir Corrêa Costa <edjandir@gmail.com>
2017-10-25 15:35:20 -0200

    Atualizando Categoria com PUT

--- src/main/java/com/edjandir/cursomc/services/CategoriaService.java
+++ src/main/java/com/edjandir/cursomc/services/CategoriaService.java
@@ -14,7 +14,7 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Categoria obj = repo.findOne(id);
 		if (obj == null) {
 			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName());
@@ -27,4 +27,8 @@ public class CategoriaService {
 		return repo.save(obj);
 	}
 	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
 }
