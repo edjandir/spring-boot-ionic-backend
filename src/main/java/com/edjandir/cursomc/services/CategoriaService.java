45984e5179c349b89e1952254b5b7bbadce25b2b
Edjandir Corrêa Costa <edjandir@gmail.com>
2017-10-25 16:12:28 -0200

    Deletando uma Categoria com DELETE

--- src/main/java/com/edjandir/cursomc/services/CategoriaService.java
+++ src/main/java/com/edjandir/cursomc/services/CategoriaService.java
@@ -1,10 +1,12 @@
 package com.edjandir.cursomc.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.edjandir.cursomc.domain.Categoria;
 import com.edjandir.cursomc.repositories.CategoriaRepository;
+import com.edjandir.cursomc.services.exception.DataIntegrityException;
 import com.edjandir.cursomc.services.exception.ObjectNotFoundException;
 
 
@@ -30,5 +32,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir categoria associada a produtos");
+		}
 	}
 }
