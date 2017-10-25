4b23c79a9ba9b73f9dcaeb9c83c554fb51499109
Edjandir Corrêa Costa <edjandir@gmail.com>
2017-10-25 19:08:57 -0200

    Validação com Bean Validation

--- src/main/java/com/edjandir/cursomc/services/CategoriaService.java
+++ src/main/java/com/edjandir/cursomc/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.edjandir.cursomc.domain.Categoria;
+import com.edjandir.cursomc.dto.CategoriaDTO;
 import com.edjandir.cursomc.repositories.CategoriaRepository;
 import com.edjandir.cursomc.services.exception.DataIntegrityException;
 import com.edjandir.cursomc.services.exception.ObjectNotFoundException;
@@ -56,5 +57,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }
